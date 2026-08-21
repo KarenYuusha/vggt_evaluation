dd75fcbe6f812de4ceeb789b6594235090ca6793
Khoa <62637453+KarenYuusha@users.noreply.github.com>
2026-08-21 16:54:51 +0700

    feat: add adapter package

---
+++ adapter/__init__.py
@@ -0,0 +1,3 @@
+from .model import FeatureAdapter, build_identity_adapter, load_adapter_checkpoint, save_adapter_checkpoint
+
+__all__ = ["FeatureAdapter", "build_identity_adapter", "load_adapter_checkpoint", "save_adapter_checkpoint"]
